5bf36d2046237ea4d93d2e432f8049a91d19677f
Serhiy Yaschuk <sergey.yaschuk@gmail.com>
2015-06-14 23:21:48 +0300

    Joined Wealth distribution and Info sharing into one simulation.

--- bin/model.rb
+++ bin/model.rb
@@ -1,21 +1,21 @@
 require 'rscape/sugarscape'
 require 'rscape/agent'
 require 'rscape/statistic'
-require 'rscape/gui/wd_preset'
+require 'rscape/gui/main_window'
 require 'rscape/gui/sugarscape_view'
 
-class WDModel < RScape::GUI::WDPreset
+class Model < RScape::GUI::MainWindow
   MIN_ROWS = 1
   MIN_COLS = 1
   MAX_ROWS = 100
   MAX_COLS = 100
   
-  slots 'start()', 'stop()', 'iterate()', 'create_model()', 'update_statistic()'
-  signals 'model_ready()'
+  slots 'start()', 'stop()', 'reset()', 'iterate()'
+  slots 'collect_statistic()', 'update_statistic()'
   
   attr_accessor :tps, :sugarscape, :population, :iterations_passed
   attr_reader :new_sugar_proc, :new_agent_proc, :sugar_placement_proc,
-  :agent_placement_proc, :iterate_proc, :view
+  :agent_placement_proc, :info_placement_proc, :iterate_proc, :view, :statistics
   
   def initialize
     super
@@ -25,28 +25,59 @@ class WDModel < RScape::GUI::WDPreset
     @sugarscape = nil
     @population = []
     @iterations_passed = 0
+    @model_ready = false
+    
     @new_sugar_proc = nil
     @new_agent_proc = nil
     @sugar_placement_proc = nil
     @agent_placement_proc = nil
+    @info_placement_proc = nil
     @iterate_proc = nil
-    @view = RScape::GUI::SugarscapeView.new 10
     
-    @view.setWindowTitle "Sugarscape view"
+    @statistics = {
+      iteration: [],
+      STAT_AGENTS_NUM => [],
+      STAT_AVG_AGE => [],
+      STAT_AVG_VISION => [],
+      STAT_AVG_WEALTH => [],
+      STAT_GINI => [],
+      STAT_INFO_PERCENT => []
+    }
     
-    connect(start_button, SIGNAL('pressed()'), self, SLOT('create_model()'))
-    connect(stop_button, SIGNAL('pressed()'), self, SLOT('stop()'))
-    connect(self, SIGNAL('model_ready()'), self, SLOT('start()'))
+    connect(control_panel.start_button, SIGNAL('pressed()'),
+            self, SLOT('start()'))
+    connect(control_panel.stop_button, SIGNAL('pressed()'),
+            self, SLOT('stop()'))
+    connect(control_panel.reset_button, SIGNAL('pressed()'),
+            self, SLOT('reset()'))
     connect(@timer, SIGNAL('timeout()'), self, SLOT('iterate()'))
-    connect(self, SIGNAL('model_ready()'), @view, SLOT('show()'))
   end
   
   def start
+    create_model unless @model_ready
+    control_panel.start_button.setEnabled false
+    control_panel.stop_button.setEnabled true
+    control_panel.reset_button.setEnabled false
+    
     @timer.start 1000 / @tps
   end
   
   def stop
+    control_panel.start_button.setEnabled true
+    control_panel.stop_button.setEnabled false
+    control_panel.reset_button.setEnabled true
+    
     @timer.stop
+  end
+  
+  def reset
+    @iterations_passed = 0
+    @model_ready = false
+    @sugarscape = nil
+    @population.clear
+    @view.clear
+    
+    clear_statistic
   end
   
   def iterate
@@ -70,6 +101,10 @@ class WDModel < RScape::GUI::WDPreset
     @agent_placement_proc = proc
   end
   
+  def set_info_placement_proc(& proc)
+    @info_placement_proc = proc
+  end
+  
   def set_iteration_proc(& proc)
     @iterate_proc = proc
   end
@@ -78,6 +113,9 @@ class WDModel < RScape::GUI::WDPreset
     total_age = @population.inject(0) { |total, agent| total + agent.age }
     total_vision = @population.inject(0) { |total, agent| total + agent.vision }
     total_wealth = @population.inject(0) { |total, agent| total + agent.wealth }
+    total_bearers = @population.count { |agent| !agent.info.empty? }
+    @population.collect { |agent| agent }
+    info_chunks_count = info_params.chunks.count
     
     social_statistic[STAT_AGENTS_NUM] = @population.size
     social_statistic[STAT_AVG_AGE] = total_age / @population.size
@@ -86,6 +124,30 @@ class WDModel < RScape::GUI::WDPreset
     economical_statistic[STAT_AVG_WEALTH] = total_wealth / @population.size
     economical_statistic[STAT_GINI] = RScape::Statistic.
       gini(@population.collect(&:wealth)).round 3
+    
+    info_spread_statistic[STAT_INFO_PERCENT] = ((total_bearers.to_f /
+      @population.size) * 100).round 2
+  end
+  
+  def collect_statistic
+    @statistics[:iteration] << @iterations_passed
+    @statistics[STAT_AGENTS_NUM] << social_statistic[STAT_AGENTS_NUM]
+    @statistics[STAT_AVG_AGE] << social_statistic[STAT_AVG_AGE]
+    @statistics[STAT_AVG_VISION] << social_statistic[STAT_AVG_VISION]
+    @statistics[STAT_AVG_WEALTH] << economical_statistic[STAT_AVG_WEALTH]
+    @statistics[STAT_GINI] << economical_statistic[STAT_GINI]
+    @statistics[STAT_INFO_PERCENT] << info_spread_statistic[STAT_INFO_PERCENT]
+  end
+  
+  def clear_statistic
+    @statistics.each { |_, array| array.clear }
+    
+    social_statistic[STAT_AGENTS_NUM] = 0
+    social_statistic[STAT_AVG_AGE] = 0
+    social_statistic[STAT_AVG_VISION] = 0
+    economical_statistic[STAT_AVG_WEALTH] = 0
+    economical_statistic[STAT_GINI] = 0
+    info_spread_statistic[STAT_INFO_PERCENT] = 0
   end
   
   private
@@ -94,8 +156,9 @@ class WDModel < RScape::GUI::WDPreset
     create_sugarscape
     plant_sugar
     settle_agents
+    spread_info
     
-    emit model_ready
+    @model_ready = true
   end
   
   def create_sugarscape
@@ -145,5 +208,9 @@ class WDModel < RScape::GUI::WDPreset
       @population << new_agent
       @view.add_agent new_agent
     end
+  end
+  
+  def spread_info
+    @population.each { |agent| @info_placement_proc.call agent }
   end
 end
--- bin/sugarscape.rb
+++ bin/sugarscape.rb
@@ -1,6 +1,8 @@
-require_relative 'wealth_distribution_model.rb'
-require_relative 'wealth_distribution_preset.rb'
+require_relative 'model.rb'
+require_relative 'simulation_preset.rb'
 require 'rscape/harvester'
+require 'rscape/neighbor'
+require 'rscape/info_sharing'
 
 def free_pos_in_fov(agent)
   distance = rand 1..agent.vision
@@ -33,6 +35,7 @@ $LOGGING = ARGV.include?("--logging") || ARGV.include?("-l")
 
 # Log colors
 GATHERING_COLOR = '#003300'
+INFO_SHARING_COLOR = '#000033'
 DEATH_COLOR = '#990000'
 
 # Model params
@@ -47,11 +50,13 @@ $AGENT_MAX_AGE ||= 0
 $SUGAR_CAPACITY ||= 0
 $SUGAR_LEVEL ||= 0
 $SUGAR_GROWTH ||= 0
+$INFO_CHUNKS ||= []
+$INITIAL_INFO_BEARERS ||= 0
 
 app = Qt::Application.new ARGV
-mw = WDModel.new
+mw = Model.new
 
-mw.tps = 5
+mw.tps = 3
 mw.sugarscape_params.rows = $ROWS_NUMBER
 mw.sugarscape_params.cols = $COLUMNS_NUMBER
 mw.sugarscape_params.agents_count = $AGENTS_NUMBER
@@ -63,11 +68,16 @@ mw.agent_params.wealth = $AGENT_INITIAL_WEALTH
 mw.agent_params.metabolism = $AGENT_METABOLISM
 mw.agent_params.vision = $AGENT_VISION
 mw.agent_params.max_age = $AGENT_MAX_AGE
+mw.info_params.chunks = $INFO_CHUNKS.map(&:to_s)
+mw.info_params.initial_bearers = $INITIAL_INFO_BEARERS
 
 mw.view.setMinimumSize(400, 400)
+mw.log.hide unless $LOGGING
 
 # Setup Agent's behavior patterns
 RScape::Agent.include RScape::Harvester
+RScape::Agent.include RScape::Neighbor
+RScape::Agent.include RScape::InfoSharing
 
 # Procedures
 mw.set_agent_creation_proc do
@@ -107,6 +117,13 @@ mw.set_sugar_placement_proc do
   end
   
   [row, col]
+end
+
+mw.set_info_placement_proc do |agent|
+  if agent.id % ((mw.population.size - 1) / $INITIAL_INFO_BEARERS) == 0
+    agent.info = $INFO_CHUNKS
+    mw.view.change_agent_color(agent.id, 0, 0, 255)
+  end
 end
 
 mw.set_iteration_proc do
@@ -165,6 +182,20 @@ mw.set_iteration_proc do
       end
     end
     
+    # Share Info
+    if !agent.info.empty?
+      agent.neighbors(mw.sugarscape).each do |neighbor|
+        agent.share_info neighbor
+        mw.view.change_agent_color(neighbor.id, 0, 0, 255)
+        
+        if $LOGGING
+          mw.log.add("<i>A:#{agent.id}</i> shared info with " \
+                     "<i>A:#{neighbor.id}</i>", color: INFO_SHARING_COLOR)
+          mw.log.add "<i>A:#{agent.id}</i> info is <i>#{agent.info}</i>"
+        end
+      end
+    end
+    
     agent.metabolize
     agent.grow
     
@@ -202,6 +233,7 @@ mw.set_iteration_proc do
     new_agent = mw.new_agent_proc.call
     row, col = mw.agent_placement_proc.call
     
+    mw.info_placement_proc.call new_agent
     new_agent.move_to(row, col)
     mw.sugarscape.place_agent new_agent
     mw.population[index] = new_agent
@@ -216,12 +248,13 @@ mw.set_iteration_proc do
   end
   
   mw.update_statistic
+  mw.collect_statistic
   
   if $LOGGING
     mw.log.add ''
   end
 end
 
-mw.setWindowTitle "Wealth distribution"
+mw.setWindowTitle "Sugarscape"
 mw.show
 app.exec
